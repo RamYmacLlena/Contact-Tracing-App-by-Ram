c6d0dc9aaea27128044f98ea0c4c84bff80b7fc5
Ram Ymac E. Llena <104246670+RamYmacLlena@users.noreply.github.com>
2022-06-09 17:25:10 +0800

    added labels and textboxes

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -29,7 +29,125 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ContactTracingAppScreeningInfo));
+            this.titilelbl = new System.Windows.Forms.Label();
+            this.namelbl = new System.Windows.Forms.Label();
+            this.agelbl = new System.Windows.Forms.Label();
+            this.addresslbl = new System.Windows.Forms.Label();
+            this.sexlbl = new System.Windows.Forms.Label();
+            this.contactnolbl = new System.Windows.Forms.Label();
+            this.nametxtbox = new System.Windows.Forms.TextBox();
+            this.agetxtbox = new System.Windows.Forms.TextBox();
+            this.addresstxtbox = new System.Windows.Forms.TextBox();
+            this.sextxtbox = new System.Windows.Forms.TextBox();
+            this.contactnotxtbox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // titilelbl
+            // 
+            this.titilelbl.AutoSize = true;
+            this.titilelbl.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.titilelbl.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.titilelbl.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.titilelbl.Location = new System.Drawing.Point(209, 18);
+            this.titilelbl.Name = "titilelbl";
+            this.titilelbl.Size = new System.Drawing.Size(418, 37);
+            this.titilelbl.TabIndex = 0;
+            this.titilelbl.Text = "COVID 19 - SCREENING FORM";
+            // 
+            // namelbl
+            // 
+            this.namelbl.AutoSize = true;
+            this.namelbl.Font = new System.Drawing.Font("Showcard Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.namelbl.Location = new System.Drawing.Point(35, 90);
+            this.namelbl.Name = "namelbl";
+            this.namelbl.Size = new System.Drawing.Size(68, 23);
+            this.namelbl.TabIndex = 1;
+            this.namelbl.Text = "NAME :";
+            // 
+            // agelbl
+            // 
+            this.agelbl.AutoSize = true;
+            this.agelbl.Font = new System.Drawing.Font("Showcard Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.agelbl.Location = new System.Drawing.Point(600, 90);
+            this.agelbl.Name = "agelbl";
+            this.agelbl.Size = new System.Drawing.Size(53, 23);
+            this.agelbl.TabIndex = 2;
+            this.agelbl.Text = "AGE :";
+            // 
+            // addresslbl
+            // 
+            this.addresslbl.AutoSize = true;
+            this.addresslbl.Font = new System.Drawing.Font("Showcard Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.addresslbl.Location = new System.Drawing.Point(35, 130);
+            this.addresslbl.Name = "addresslbl";
+            this.addresslbl.Size = new System.Drawing.Size(97, 23);
+            this.addresslbl.TabIndex = 3;
+            this.addresslbl.Text = "ADDRESS :";
+            // 
+            // sexlbl
+            // 
+            this.sexlbl.AutoSize = true;
+            this.sexlbl.Font = new System.Drawing.Font("Showcard Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.sexlbl.Location = new System.Drawing.Point(601, 130);
+            this.sexlbl.Name = "sexlbl";
+            this.sexlbl.Size = new System.Drawing.Size(52, 23);
+            this.sexlbl.TabIndex = 4;
+            this.sexlbl.Text = "SEX :";
+            // 
+            // contactnolbl
+            // 
+            this.contactnolbl.AutoSize = true;
+            this.contactnolbl.Font = new System.Drawing.Font("Showcard Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.contactnolbl.Location = new System.Drawing.Point(35, 170);
+            this.contactnolbl.Name = "contactnolbl";
+            this.contactnolbl.Size = new System.Drawing.Size(178, 23);
+            this.contactnolbl.TabIndex = 5;
+            this.contactnolbl.Text = "CONTACT NUMBER :";
+            // 
+            // nametxtbox
+            // 
+            this.nametxtbox.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.nametxtbox.Location = new System.Drawing.Point(109, 90);
+            this.nametxtbox.Multiline = true;
+            this.nametxtbox.Name = "nametxtbox";
+            this.nametxtbox.Size = new System.Drawing.Size(470, 23);
+            this.nametxtbox.TabIndex = 6;
+            // 
+            // agetxtbox
+            // 
+            this.agetxtbox.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.agetxtbox.Location = new System.Drawing.Point(659, 90);
+            this.agetxtbox.Multiline = true;
+            this.agetxtbox.Name = "agetxtbox";
+            this.agetxtbox.Size = new System.Drawing.Size(106, 23);
+            this.agetxtbox.TabIndex = 7;
+            // 
+            // addresstxtbox
+            // 
+            this.addresstxtbox.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.addresstxtbox.Location = new System.Drawing.Point(138, 130);
+            this.addresstxtbox.Multiline = true;
+            this.addresstxtbox.Name = "addresstxtbox";
+            this.addresstxtbox.Size = new System.Drawing.Size(441, 23);
+            this.addresstxtbox.TabIndex = 8;
+            // 
+            // sextxtbox
+            // 
+            this.sextxtbox.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.sextxtbox.Location = new System.Drawing.Point(659, 130);
+            this.sextxtbox.Multiline = true;
+            this.sextxtbox.Name = "sextxtbox";
+            this.sextxtbox.Size = new System.Drawing.Size(106, 23);
+            this.sextxtbox.TabIndex = 9;
+            // 
+            // contactnotxtbox
+            // 
+            this.contactnotxtbox.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.contactnotxtbox.Location = new System.Drawing.Point(219, 170);
+            this.contactnotxtbox.Multiline = true;
+            this.contactnotxtbox.Name = "contactnotxtbox";
+            this.contactnotxtbox.Size = new System.Drawing.Size(360, 23);
+            this.contactnotxtbox.TabIndex = 10;
             // 
             // ContactTracingAppScreeningInfo
             // 
@@ -40,13 +158,38 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 532);
+            this.Controls.Add(this.contactnotxtbox);
+            this.Controls.Add(this.sextxtbox);
+            this.Controls.Add(this.addresstxtbox);
+            this.Controls.Add(this.agetxtbox);
+            this.Controls.Add(this.nametxtbox);
+            this.Controls.Add(this.contactnolbl);
+            this.Controls.Add(this.sexlbl);
+            this.Controls.Add(this.addresslbl);
+            this.Controls.Add(this.agelbl);
+            this.Controls.Add(this.namelbl);
+            this.Controls.Add(this.titilelbl);
             this.HelpButton = true;
             this.Name = "ContactTracingAppScreeningInfo";
-            this.Text = "COVID 19 - SCREENING INFORMATION";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "CONTACT TRACING FORM";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label titilelbl;
+        private Label namelbl;
+        private Label agelbl;
+        private Label addresslbl;
+        private Label sexlbl;
+        private Label contactnolbl;
+        private TextBox nametxtbox;
+        private TextBox agetxtbox;
+        private TextBox addresstxtbox;
+        private TextBox sextxtbox;
+        private TextBox contactnotxtbox;
     }
 }
