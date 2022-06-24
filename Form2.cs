a160da9249b615927d803137ecf85896666e0081
Ram Ymac E. Llena <104246670+RamYmacLlena@users.noreply.github.com>
2022-06-24 16:28:52 +0800

    trying to make an array

--- Form2.cs
+++ Form2.cs
@@ -35,7 +35,6 @@ namespace Contact_Tracing_App
 
         private void submitbtn_Click(object sender, EventArgs e)
         {
-
             string FirstDoseAnswer = null;
             string SecondDoseAnswer = null;
             string TestPositiveAnswer = null;
@@ -115,29 +114,25 @@ namespace Contact_Tracing_App
                 MessageBox.Show("Please answer the blank sections!");
             }
             else
-                    {
-                        StreamWriter file = File.AppendText("D:\\DESKTOP\\Ram Ymac\\EDUCATION\\OOP\\Contact Tracing App Infos\\Contact Tracing Information.txt");
-                        file.WriteLine("Name : " + nametxtbox.Text);
-                        file.WriteLine("Address : " + addresstxtbox.Text);
-                        file.WriteLine("Contact Number: " + contactnotxtbox.Text);
-                        file.WriteLine("Age : " + agetxtbox.Text);
-                        file.WriteLine("Sex : " + sextxtbox.Text);
-                        file.WriteLine("Temperature : " + temperaturetxtbox.Text);
-                        file.WriteLine("First Dose : " + FirstDoseAnswer);
-                        file.WriteLine("Second Dose : " + SecondDoseAnswer);
-                        file.WriteLine("Have had contact with someone who tested postive? " + TestPositiveAnswer);
-                        file.WriteLine(haveyouexprnlbl.Text + " ");
-                        file.WriteLine("1. Fever? " + FeverAnswer);
-                        file.WriteLine("2. Loss of Taste? " + LossofTasteAnswer);
-                        file.WriteLine("3. Loss of Smell? " + LossofSmellAnswer);
-                        file.Close();
-            }
             {
+                var tracing = new[]
+{
+                 ("Name", nametxtbox.Text), ("Address", addresstxtbox.Text), ("Contact Number", contactnotxtbox.Text), ("Age", agetxtbox.Text),  ("Sex", sextxtbox.Text), ("Temperature", temperaturetxtbox.Text), ("First Dose", FirstDoseAnswer), ("Second Dose", SecondDoseAnswer), ("Have had contact with someone who tested postive", TestPositiveAnswer), ("haveyouexprnlbl", " "), ("Fever", FeverAnswer), ("Loss of Taste", LossofTasteAnswer), ("Loss of Smell", LossofSmellAnswer)
+            };
+
+                StreamWriter file = File.AppendText("D:\\DESKTOP\\Ram Ymac\\EDUCATION\\OOP\\Contact Tracing App Infos\\Contact Tracing Information.txt");
+                for (int i = 0; i < 13; i++)
+                {
+                    file.WriteLine(tracing[i]);
+                }
+
+                file.Close();
                 MessageBox.Show("Registration Successful!");
+                ContactTracingAppScreeningInfo NewForm = new ContactTracingAppScreeningInfo();
+                NewForm.Show();
+                this.Dispose(false);
             }
-            ContactTracingAppScreeningInfo NewForm = new ContactTracingAppScreeningInfo();
-            NewForm.Show();
-            this.Dispose(false);
+
 
         }
 
